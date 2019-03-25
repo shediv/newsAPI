523baeafba6d5c89f506b36daee44cfd78f83f0b
shediv <videshhedge@gmail.com>
2019-03-25 11:07:12 +0530

    Added new feedTest API

--- index.js
+++ index.js
@@ -72,24 +72,81 @@ app.get("/feeds", function(req, res){
 
 app.get("/feedTest", function(req, res){
 
-  let topNews = `<?xml version="1.0" encoding="UTF-8"?>
-  <rss version="2.0"
-      xmlns:content="http://purl.org/rss/1.0/modules/content/"
-      xmlns:media="http://search.yahoo.com/mrss/">
-      <channel>
-          <lastBuildDate>Fri, 23 Jan 2015 23:26:19 +0000</lastBuildDate>
-          <title>Google News RSS Examples</title>
-          <description>Google News RSS Examples</description>
-          <link>http://google-news-examples.blogspot.com/</link>
+  let topNews = `<rss version="2.0">
+  <channel>
+      <title>FeedForAll Sample Feed</title>
+      <description>
+      RSS is a fascinating technology. The uses for RSS are expanding daily. Take a closer look at how various industries are using the benefits of RSS in their businesses.
+      </description>
+      <link>http://www.feedforall.com/industry-solutions.htm</link>
+      <category domain="www.dmoz.com">
+      Computers/Software/Internet/Site Management/Content Management
+      </category>
+      <copyright>Copyright 2004 NotePage, Inc.</copyright>
+      <docs>http://blogs.law.harvard.edu/tech/rss</docs>
+      <language>en-us</language>
+      <lastBuildDate>Tue, 19 Oct 2004 13:39:14 -0400</lastBuildDate>
+      <managingEditor>marketing@feedforall.com</managingEditor>
+      <pubDate>Tue, 19 Oct 2004 13:38:55 -0400</pubDate>
+      <webMaster>webmaster@feedforall.com</webMaster>
+      <generator>FeedForAll Beta1 (0.0.1.8)</generator>
+      <image>
+          <url>http://www.feedforall.com/ffalogo48x48.gif</url>
+          <title>FeedForAll Sample Feed</title>
+          <link>http://www.feedforall.com/industry-solutions.htm</link>
+          <description>FeedForAll Sample Feed</description>
+          <width>48</width>
+          <height>48</height>
+      </image>
+      <item>
+          <title>RSS Resources</title>
+          <description>
+            <p>
+                Be sure to take a look at some of our favorite RSS Resources
+                <a href="http://www.rss-specifications.com">
+                    RSS Specifications
+                </a>
+                <a href="http://www.blog-connection.com">
+                    Blog Connection
+                </a>
+            </p>
+          </description>
+          <link>http://www.feedforall.com</link>
+          <pubDate>Tue, 26 Oct 2004 14:01:01 -0500</pubDate>
+      </item>
           <item>
-              <guid isPermaLink="false">sample-post-478956386763692725</guid>
-              <pubDate>Fri, 23 Jan 2015 23:17:00 +0000</pubDate>
-              <title>Sample RSS post #1</title>
-              <description>An example RSS article</description>
-              <content:encoded><![CDATA[<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras egestas mattis lectus eget porttitor. Nunc iaculis luctus libero, quis viverra mi ultricies sed. Nulla pellentesque dui sed maximus tristique. Sed tempor pulvinar ex in mattis.</p><p><a href="http://www.google.com/">Sample Link</a></p><p><a href="https://www.google.com/images/srpr/logo11w.png"><img border="0" height="113" src="https://www.google.com/images/srpr/logo11w.png" width="320" /></a></p><p>Nulla dictum magna orci, et accumsan velit elementum sit amet. Vestibulum egestas, nulla nec facilisis iaculis, elit metus molestie mi, et vulputate enim eros vitae sem. Aliquam eget sagittis dui. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam mattis ex sit amet lectus semper tempus.</p><iframe width="560" height="315" src="//www.youtube.com/embed/200E9e8_hHY" frameborder="0" allowfullscreen></iframe>]]></content:encoded> <link>http://google-news-examples.blogspot.com/2015/01/sample-post.html</link>
-              <author>mail@example.com (Lauren Ipson)</author>
-          </item>
-      </channel>
+          <title>Recommended Desktop Feed Reader Software</title>
+          <description>
+          <p>
+              Be sure to take a look at some of our favorite RSS Resources
+              <a href="http://www.rss-specifications.com">
+                  RSS Specifications
+              </a>
+              <a href="http://www.blog-connection.com">
+                  Blog Connection
+              </a>
+          </p>
+          </description>
+          <link>http://www.feedforall.com/feedforall-partners.htm</link>
+          <pubDate>Tue, 26 Oct 2004 14:03:25 -0500</pubDate>
+      </item>
+      <item>
+          <title>Recommended Web Based Feed Reader Software</title>
+          <description>
+          <p>
+              Be sure to take a look at some of our favorite RSS Resources
+              <a href="http://www.rss-specifications.com">
+                  RSS Specifications
+              </a>
+              <a href="http://www.blog-connection.com">
+                  Blog Connection
+              </a>
+          </p>
+          </description>
+          <link>http://www.feedforall.com/feedforall-partners.htm</link>
+          <pubDate>Tue, 26 Oct 2004 14:06:44 -0500</pubDate>
+      </item>
+  </channel>
   </rss>`;
 
   return res.type('application/xml').send(topNews);
