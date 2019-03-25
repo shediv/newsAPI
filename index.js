a40564c54ed12ab58356d05a493adae36c58a38d
shediv <videshhedge@gmail.com>
2019-03-25 15:37:05 +0530

    removed image

--- index.js
+++ index.js
@@ -67,7 +67,6 @@ app.get("/test", function(req, res) {
             link: news.url,
             description: news.description,
             content: news.content,
-            image: news.urlToImage,
             pubDate: news.publishedAt
         })
       });
